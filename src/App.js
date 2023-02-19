c34a8c776a7e20ca28b0757e5ed884fdac123ff7
Fevzi Atanoğlu <fevziatan2@gmail.com>
2023-02-19 21:17:32 +0300

    Menu update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AboutMe from "./components/AboutMe/aboutme";
 import GetContact from "./components/GetContact/getcontact";
 import MenuButton from "./components/Menu/menuButton";
+import Menu from "./components/Menu/menu";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path={""} element={<MainScreen />} />
           <Route path="aboutme" element={<AboutMe />} />
           <Route path="getcontact" element={<GetContact />} />
+          <Route path="menu" element={<Menu />} />
 
         </Routes>
       </BrowserRouter>
--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,7 +1,11 @@
 
 function Menu(){
-    return <div>
-        This is Menu
+    return <div className="main-container">
+        <div className="main-content-50 menu-links">
+            <a href="/" className="menu-link">Home</a>
+            <a href="/aboutme" className="menu-link">About Me</a>
+            <a href="/getcontact" className="menu-link">Get Contact</a>
+        </div>
     </div>
 }
 
