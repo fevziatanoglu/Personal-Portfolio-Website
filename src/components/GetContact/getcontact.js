f9d527dd707b4792aeed6f55e2d6ba2a2c0b2537
Fevzi Atanoğlu <fevziatan2@gmail.com>
2023-02-20 16:58:19 +0300

    Added get contact session with emailjs

--- src/components/GetContact/getcontact.js
+++ src/components/GetContact/getcontact.js
@@ -1,9 +1,32 @@
+import { useRef } from "react";
 import MenuButton from "../Menu/menuButton";
+import emailjs from "@emailjs/browser"
 
-function GetContact(){
+import "./getcontact.css"
+
+function GetContact() {
+
+    const emailForm = useRef();
+
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm("service_ikc7gy8", "template_r20b7nq", emailForm.current, "EjKtsnAZGVYwTgOfo").then(response => {alert("Email sent. I will be return soon:)");}).catch(error => alert("There is an error please try later :/ ."));
+        console.log(emailForm.current);
+    }
+
     return <div className="main-container">
-        <MenuButton/>
-        This is get contact
+        <MenuButton />
+        <div className="main-content-50">
+
+
+            <form ref={emailForm} onSubmit={sendEmail}>
+                <label>Name <input type="text" name="user_name" placeholder="Name" /></label>
+                <label>Email  <input type="email" name="user_email" placeholder="Email" /></label>
+                <label>Message<textarea name="message" placeholder="Your Message" /></label>
+                <button type="submit">Send</button>
+            </form>
+        </div>
     </div>
 }
 
