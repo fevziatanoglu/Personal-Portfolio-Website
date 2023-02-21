3f5d548967aec230082716d722bbdc2666fc0a54
Fevzi Atanoğlu <fevziatan2@gmail.com>
2023-02-21 19:24:16 +0300

    get contact style update and favicon

--- src/components/GetContact/getcontact.js
+++ src/components/GetContact/getcontact.js
@@ -11,22 +11,26 @@ function GetContact() {
 
     const sendEmail = (e) => {
         e.preventDefault();
-        emailjs.sendForm("service_ikc7gy8", "template_r20b7nq", emailForm.current, "EjKtsnAZGVYwTgOfo").then(response => {alert("Email sent. I will be return soon:)");}).catch(error => alert("There is an error please try later :/ ."));
+        emailjs.sendForm("service_ikc7gy8", "template_r20b7nq", emailForm.current, "EjKtsnAZGVYwTgOfo").then(response => { alert("Email sent. I will be return soon:)"); }).catch(error => alert("There is an error please try later :/ ."));
         console.log(emailForm.current);
     }
 
     return <div className="main-container">
         <MenuButton />
-        <div className="main-content-50">
 
-
-            <form ref={emailForm} onSubmit={sendEmail}>
+        <form ref={emailForm} onSubmit={sendEmail}>
+            <div className="main-content-50 form-content">
                 <label>Name <input type="text" name="user_name" placeholder="Name" /></label>
                 <label>Email  <input type="email" name="user_email" placeholder="Email" /></label>
+            </div>
+
+            <div className="main-content-50 form-content">
                 <label>Message<textarea name="message" placeholder="Your Message" /></label>
-                <button type="submit">Send</button>
-            </form>
-        </div>
+                <button className="send-button" type="submit">Send</button>
+            </div>
+
+        </form>
+
     </div>
 }
 
